e421706f45fdcba45e9231bf6f34ee1c0c283108
samusesapple <uinus.shg@uinus.co.kr>
2024-12-20 23:27:24 +0900

    feat: AnimationView 컴포넌트 추가

---
+++ src/components/AnimationView/index.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useRive, UseRiveParameters } from 'rive-react';
+
+interface AnimationViewProps {
+  params: UseRiveParameters;
+  size?: {
+    width: number;
+    height: number;
+  };
+}
+
+const AnimationView: React.FC<AnimationViewProps> = ({ params, size = { width: 600, height: 500 } }) => {
+  const { RiveComponent, rive } = useRive(params);
+
+  useEffect(() => {
+    if (rive && params?.animations) {
+      rive.reset();
+      rive.play(params.animations[0]);
+    }
+  }, [rive]);
+
+  return (
+    <div>
+      <div style={{ width: size.width, height: size.height }}>
+        <RiveComponent />
+      </div>
+    </div>
+  );
+};
+
+export default AnimationView;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,19 @@
 import styled from 'styled-components';
 import ClickAnimationView from '@@components/AnimationView/ClickAnimationView';
+import clickAnimation1 from '@@assets/images/clickAnimation1.riv';
 import animation1 from '@@assets/images/animation1.riv';
+import AnimationView from '@@components/AnimationView';
+
 const StyledHome = styled.div``;
 
 function Home() {
   return (
     <StyledHome>
       <ClickAnimationView
-        params={{ src: animation1, autoplay: true, animations: ['Timeline 1', 'Timeline 2', 'Timeline 3', 'Timeline 4'] }}
+        params={{ src: clickAnimation1, autoplay: true, animations: ['Timeline 1', 'Timeline 2', 'Timeline 3', 'Timeline 4'] }}
         maxTouchCount={4}
       />
+      <AnimationView params={{ src: animation1, autoplay: true }} />
     </StyledHome>
   );
 }
