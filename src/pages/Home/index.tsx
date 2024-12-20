2da64212b8c84d2d96a20a2af9f07b26409052ff
samusesapple <uinus.shg@uinus.co.kr>
2024-12-20 23:21:42 +0900

    feat: ClickAnimationView 컴포넌트 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
-
+import ClickAnimationView from '@@components/AnimationView/ClickAnimationView';
+import animation1 from '@@assets/images/animation1.riv';
 const StyledHome = styled.div``;
 
 function Home() {
-  return <StyledHome>홈 화면입니다.</StyledHome>;
+  return (
+    <StyledHome>
+      <ClickAnimationView
+        params={{ src: animation1, autoplay: true, animations: ['Timeline 1', 'Timeline 2', 'Timeline 3', 'Timeline 4'] }}
+        maxTouchCount={4}
+      />
+    </StyledHome>
+  );
 }
 
 export default Home;
