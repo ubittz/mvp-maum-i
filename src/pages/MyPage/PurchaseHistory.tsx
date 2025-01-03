e9f57147505e6bb3a15bdfbc6845665b6c601308
성현관 <uinus.shg@uinus.co.kr>
2025-01-03 18:22:48 +0900

    ui: 결제내역 base 작업

--- src/pages/MyPage/PurchaseHistory.tsx
+++ src/pages/MyPage/PurchaseHistory.tsx
@@ -1,5 +1,30 @@
-function PurchaseHistory() {
-  return <div>PurchaseHistory</div>;
-}
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
+import FullScreen from '@@components/FullScreen';
+import MyPageContainer from '@@pages/MyPage/parts/MyPageContainer';
+
+const StyledPurchaseHistory = styled(FullScreen)`
+  overflow-y: hidden;
+
+  & > div {
+    flex: 0 0 auto;
+  }
+`;
+
+function PurchaseHistory() {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <StyledPurchaseHistory gnb>
+      <MyPageContainer title='결제 내역' onBack={handleBack}>
+        <div>PurchaseHistory</div>
+      </MyPageContainer>
+    </StyledPurchaseHistory>
+  );
+}
 export default PurchaseHistory;
